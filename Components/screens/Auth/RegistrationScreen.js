6968a76fde247b4e880e9c543f3249e1e455f548
Shuayb Badoolah <shuayb.b99@gmail.com>
2020-04-21 16:31:41 -0400

    starting registration page

--- Components/screens/Auth/RegistrationScreen.js
+++ Components/screens/Auth/RegistrationScreen.js
@@ -1,18 +1,35 @@
 import React , {Component } from "react"
-import { StyleSheet, Text, View , Button, Image} from 'react-native';
+import { StyleSheet, Text, View , Button, Image, TouchableOpacity,} from 'react-native';
 
 
 export default function RegistrationScreen({ navigation }){
 
     return(
-        <View>
-        <Text>RegistrationScreen</Text>
-        <Button
-          title="React Native by Example"
-          onPress={() =>
-            navigation.push("Profile")
-          }
-        />       
-        </View>
+      <View style={styles.emailAndPassword}>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="email"
+          onChangeText={(email) => setEmail(email)}
+          value={email}
+        />
+
+        <TextInput
+          placeholder="password"
+          style={styles.input}
+          secureTextEntry
+          onChangeText={(password) => setPassword(password)}
+          value={password}
+        />
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={onBottomPress}
+        >
+          <Text style={styles.buttonText}>Sign Up!</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.errorText}>{error}</Text>
+      </View>
+    </View>
         );
 }
