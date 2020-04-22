c5232ba172b6dbb9c9efd0e5d93afab9386b8c5e
SnehPatel <sneh0107@gmail.com>
2020-04-22 19:54:15 -0400

    Added prices and timeleft

--- Components/screens/App/HomeScreen.js
+++ Components/screens/App/HomeScreen.js
@@ -4,11 +4,11 @@ import { FlatGrid, SectionGrid } from "react-native-super-grid"
 
 export default function HomeScreen({ navigation }) {
 
-  const items = [{ name: "Clothes", pic: require("../../../assets/clothes.png") },
-  { name: "Bracelet", pic: require("../../../assets/bracelet-1.png") },
-  { name: "Shoes", pic: require("../../../assets/shoe.png") },
-  { name: "Pants", pic: require("../../../assets/trousers.png") },
-  { name: "Watch", pic: require("../../../assets/watch.png") },
+  const items = [{ name: "Clothes", price: "$13.99", timeLeft: "1h 3min", pic: require("../../../assets/clothes.png") },
+  { name: "Ring", price: "$100000.00", timeLeft: "10h 0min", pic: require("../../../assets/bracelet-1.png") },
+  { name: "Shoes", price: "$52.97", timeLeft: "3h 45min", pic: require("../../../assets/shoe.png") },
+  { name: "Pants", price: "$7.50", timeLeft: "7h 24min", pic: require("../../../assets/trousers.png") },
+  { name: "Watch", price: "$999.99", timeLeft: "2h 37min", pic: require("../../../assets/watch.png") },
   ];
 
   return (
@@ -28,6 +28,8 @@ export default function HomeScreen({ navigation }) {
           <View style={[styles.itemContainer]}>
             <Image source={item.pic} style={{ width: 150, height: 100, }} resizeMode="contain" />
             <Text style={styles.itemName}>{item.name}</Text>
+            <Text style={styles.itemPrice}>{item.price}</Text>
+            <Text style={styles.itemPrice}>{item.timeLeft} left</Text>
           </View>
         )}
       />
@@ -64,7 +66,7 @@ const styles = StyleSheet.create({
     borderColor: "#e8c309",
     borderWidth: 2,
     padding: 10,
-    height: 150,
+    height: 185,
     alignItems: "center",
     backgroundColor: "#e5e5e5"
   },
@@ -74,9 +76,9 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     paddingTop: 7
   },
-  itemCode: {
+  itemPrice: {
     fontWeight: '600',
-    fontSize: 12,
-    color: '#fff',
+    fontSize: 16,
+    color: '#fca311',
   },
 });
