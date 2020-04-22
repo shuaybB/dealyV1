59005c369464310e97fc1562d4202c743ce1ebac
Shuayb Badoolah <shuayb.b99@gmail.com>
2020-04-22 13:09:22 -0400

    Update ProfileScreen.js

--- Components/screens/App/ProfileScreen.js
+++ Components/screens/App/ProfileScreen.js
@@ -2,16 +2,11 @@ import React, { Component , useEffect, useState} from "react"
 import { StyleSheet, Text, View, Button, Image, TextInput, TouchableOpacity } from 'react-native';
 import { Avatar } from "react-native-elements"
 import firebase from "firebase";
+import { useFocusEffect } from '@react-navigation/native';
 
 export default function ProfileScreen({ navigation }) {
   const { currentUser } = firebase.auth()
-  const [email, setEmail] = useState("");
-  const [nickname, setNickname] = useState("");
-  useEffect(() => {
-    console.log("ran")
-    setEmail(currentUser.email)
-    setNickname(currentUser.displayName)
-  });
+
 
   const onBtnEdit = () => {
     navigation.navigate('EditProfile')
@@ -28,8 +23,8 @@ export default function ProfileScreen({ navigation }) {
           uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
         }} size="xlarge" rounded showEditButton containerStyle={{ marginTop: -50 }} />
 
-        <Text style={styles.emailName}>{email}</Text>
-        <Text style={styles.emailName}>{nickname}</Text>
+        <Text style={styles.emailName}>{currentUser.email}</Text>
+        <Text style={styles.emailName}>{currentUser.displayName}</Text>
 
       </View>
       <TouchableOpacity style={styles.buttonContainer} onPress={onBtnEdit}>
